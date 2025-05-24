a4bfe421dc17694b13799ac1728215ab7ce24d3d
PRINCE MD <princerwanda2004@gmail.com>
2025-05-24 13:47:01 +0300

    Update set.js

--- set.js
+++ set.js
@@ -14,7 +14,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "255697929318",
+    OWNER_NAME: process.env.OWNER_NAME || "255623672733",
     NUMERO_OWNER : process.env.NUMERO_OWNER || " PRINCE",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
